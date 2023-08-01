05c969d4e8a90e67f7d5748cc82a02c8da4564aa
russell258 <duckie258@gmail.com>
2023-08-02 02:09:33 +0800

    day 31

--- src/app/components/fruits/fruits.component.ts
+++ src/app/components/fruits/fruits.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fruits',
   templateUrl: './fruits.component.html',
   styleUrls: ['./fruits.component.css']
 })
-export class FruitsComponent {
+export class FruitsComponent implements OnInit{
   fruit: string = 'Durian';
   fullName: string = 'Darryl Ng';
   // xx-small, x-small, small, medium,large,x-large,xx-large
   fontSize: string = 'xxx-large';
   fontWeight: string = 'bold';
+
+  fruits: [{name: string, quantity: number}] = [{name:'apple',quantity:50}];
+
+  ngOnInit(): void {
+    this.fruits.push({name:'mango',quantity:10});
+    this.fruits.push({name:'durian',quantity:10});
+    this.fruits.push({name:'avocado',quantity:25});
+
+  }
 
   testClick(){
     console.log('Button Test Clicked');
